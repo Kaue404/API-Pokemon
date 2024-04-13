3eff20aff9eb99698590f61ff565d675bf720ffe
Kauê José <jkaue986@gmail.com>
2024-04-13 14:30:43 -0300

    quarto commit

--- src/pages/Error/index.js
+++ src/pages/Error/index.js
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom'
 
-function Error(){
-    return(
+function Error() {
+    return (
         <div>
             <h2>Ops, a página que você está tentando acessar não existe!</h2>
             <br />
--- src/pages/Sobre/index.js
+++ src/pages/Sobre/index.js
@@ -8,7 +8,7 @@ function Sobre() {
 
             <h3>Esse site foi feito para um trabalho de Faculdade (FATEC), na aula de desevolvimento web III, <br />
                 usando uma API de pokemon e React.js. No site você pode pesquisar e achar qualquer<br />
-                pokemon e caso queira saber como instalar ou utilizar React.js basta acessar: </h3> <br />
+                pokemon e caso queira saber como ter ou utilizar o site basta acessar: </h3> <br />
 
             <a href="https://github.com/Kaue404/API-Pokemon">https://github.com/Kaue404/API-Pokemon</a>
         </div>
